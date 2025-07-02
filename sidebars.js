383ee458fe3e2fe40e7b72519e7fc0a720e9cc17
st-IO-OI <59336563+st-IO-OI@users.noreply.github.com>
2025-07-02 14:23:31 +0900

    07021400

--- sidebars.js
+++ sidebars.js
@@ -24,6 +24,7 @@ const sidebars = {
       collapsed: false,
       items: [
     'index',             // docs/intro.md
+  // 一般用語
     {
       type: 'category',
       label: '宇宙開発一般',
@@ -50,6 +51,7 @@ const sidebars = {
         },
       ],
     },
+  // ロケット
     {
       type: 'category',
       label: 'ロケット',
@@ -138,6 +140,7 @@ const sidebars = {
 
       ],
     },
+  // 人工衛星
     {
       type: 'category',
       label: '人工衛星',
@@ -157,6 +160,15 @@ const sidebars = {
         },
         {
           type: 'category',
+          label: '人工衛星のシステム',
+          collapsed: true,
+          items: [
+            'satellite/system/orbit-insertion',
+            'satellite/system/satellite-bus',
+          ],
+        },
+        {
+          type: 'category',
           label: '実際の人工衛星一覧',
           collapsed: true,
           items: [
@@ -166,6 +178,7 @@ const sidebars = {
         },
       ],
     },
+  // 探査機
     {
       type: 'category',
       label: '探査機',
@@ -173,12 +186,40 @@ const sidebars = {
       collapsed: true,
       items: [
         'explorer/space-probe',
-        'explorer/mission/hayabusa',
-        'explorer/mission/hayabusa2',
-        'explorer/technology/sample-return',
-        'explorer/technology/flyby',
+        {
+          type: 'category',
+          label: '宇宙探査機の技術',
+          collapsed: true,
+          items: [
+            'explorer/technology/sample-return',
+            'explorer/technology/rendezvous',
+            'explorer/technology/flyby',
+            'explorer/technology/swingby',
+            'explorer/technology/capsule',
+            'explorer/technology/reentry',
+            'explorer/technology/heat-shield',
+            'explorer/technology/hovering',
+            'explorer/technology/lander',
+            'explorer/technology/payload-instrument',
+          ],
+        },
+        {
+          type: 'category',
+          label: '実際の宇宙探査機のミッション',
+          collapsed: true,
+          items: [
+            'explorer/mission/hayabusa',
+            'explorer/mission/hayabusa2',
+            'explorer/mission/moon-probe',
+            'explorer/mission/new-horizons',
+            'explorer/mission/voyager',
+            'explorer/mission/rosetta',
+            'explorer/mission/pioneer',
+          ],
+        },
       ],
     },
+  // 軌道
     {
       type: 'category',
       label: '軌道',
@@ -186,32 +227,114 @@ const sidebars = {
       collapsed: true,
       items: [
         'orbit/orbit',
-        'orbit/mechanics/orbital-elements',
-        'orbit/mechanics/first-cosmic-velocity',
-        'orbit/type/circular-orbit',
-        'orbit/type/geostationary-orbit',
-        'orbit/type/sun-synchronous-orbit',
+        {
+          type: 'category',
+          label: '軌道力学',
+          collapsed: true,
+          items: [
+            'orbit/mechanics/delta-v-budget',
+            'orbit/mechanics/orbital-elements',
+            'orbit/mechanics/eccentricity',
+            'orbit/mechanics/apogee',
+            'orbit/mechanics/perigee',
+            'orbit/mechanics/lagrange-point',
+            'orbit/mechanics/inclination',
+            'orbit/mechanics/escape-velocity',
+            'orbit/mechanics/first-cosmic-velocity',
+          ],
+        },
+        {
+          type: 'category',
+          label: '軌道の種類',
+          collapsed: true,
+          items: [
+            'orbit/type/circular-orbit',
+            'orbit/type/geostationary-orbit',
+            'orbit/type/sun-synchronous-orbit',
+            'orbit/type/elliptical-orbit',
+            'orbit/type/free-return-trajectory',
+            'orbit/type/geocentric-orbit',
+            'orbit/type/hohmann-transfer-orbit',
+            'orbit/type/low-earth-orbit',
+            'orbit/type/parking-orbit',
+            'orbit/type/polar-orbit',
+            'orbit/type/suborbital-orbit',
+            'orbit/type/transfer-orbit',
+          ],
+        },
+        {
+          type: 'category',
+          label: '軌道の運用',
+          collapsed: true,
+          items: [
+            'satellite/operation/nodal-precession',
+            'satellite/operation/orbital-decay',
+            'satellite/operation/orbit-escape',
+            'satellite/operation/orbital-insertion',
+            'satellite/operation/orbital-resonance',
+          ],
+        },
       ],
     },
+  // 制御
     {
       type: 'category',
       label: '制御',
       link: { type: 'doc', id: 'category/control' },
       collapsed: true,
       items: [
-        'control/technology/attitude-control',
-        'control/technology/rendezvous-docking',
+        {
+          type: 'category',
+          label: '制御技術',
+          collapsed: true,
+          items: [
+            'control/technology/attitude-control',
+            'control/technology/rendezvous-docking',
+          ],
+        },
       ],
     },
+  // 通信
     {
       type: 'category',
       label: '通信',
       link: { type: 'doc', id: 'category/communication' },
       collapsed: true,
       items: [
-        'communication/technology/antenna',
-        'communication/technology/communication-delay',
-        'communication/system/ground-station',
+        'communication/communication',
+        {
+          type: 'category',
+          label: '通信技術',
+          collapsed: true,
+          items: [
+            'communication/technology/antenna',
+            'communication/technology/communication-delay',
+            'communication/technology/bit-rate',
+            'communication/technology/frequency-band',
+            'communication/technology/ka-band',
+            'communication/technology/x-band',
+            'communication/technology/s-band',
+          ],
+        },
+        {
+          type: 'category',
+          label: '通信システム',
+          collapsed: true,
+          items: [
+            'commnication/system/command',
+            'commnication/system/telemetry',
+            'commnication/system/ground-station',
+            'commnication/system/dsn',
+          ],
+        },
+        {
+          type: 'category',
+          label: '通信手法',
+          collapsed: true,
+          items: [
+            'control/type/optical-communication',
+          ],
+        },
       ],
     }, 
       ],
