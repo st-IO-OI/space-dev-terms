8165557e246a62e6536bd97db0266e8a5ab1c534
st-IO-OI <59336563+st-IO-OI@users.noreply.github.com>
2025-07-03 23:28:45 +0900

    07032300

--- src/components/AdsenseBanner.js
+++ src/components/AdsenseBanner.js
@@ -24,8 +24,8 @@ export default function AdBanner({ type = 'default' }) {
           border: '1px solid #95a5a6',
           borderRadius: '0.75rem',
           backgroundColor: '#FFFFFF',
-          width: 504,
-          margin: '0 auto',
+          width: 280,
+          margin: 0,
           padding: 5,
           textAlign: 'center',
           overflow: 'hidden',
@@ -34,27 +34,27 @@ export default function AdBanner({ type = 'default' }) {
         <table>
           <tbody>
             <tr>
-              <td style={{ width: 240 }}>
+              <td style={{ width: 128 }}>
                 <a
-                  href="https://hb.afl.rakuten.co.jp/ichiba/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16497722%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIzMDB4MzAwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+                  href="https://hb.afl.rakuten.co.jp/ichiba/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16497722%2F&link_type=picttext&ut=..."
                   target="_blank"
                   rel="nofollow sponsored noopener"
                   style={{ wordWrap: 'break-word' }}
                 >
                   <img
-                    src="https://hbb.afl.rakuten.co.jp/hgb/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?me_id=1213310&item_id=20165144&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fbook%2Fcabinet%2F6397%2F9784140886397.jpg%3F_ex%3D300x300&s=300x300&t=picttext"
+                    src="https://hbb.afl.rakuten.co.jp/hgb/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?me_id=1213310&item_id=20165144&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fbook%2Fcabinet%2F6397%2F9784140886397.jpg%3F_ex%3D128x128&s=128x128&t=picttext"
                     border="0"
-                    alt="はやぶさ2　最強ミッションの真実"
                     style={{ margin: 2 }}
+                    alt="はやぶさ2書籍"
                     title="はやぶさ2　最強ミッションの真実"
                   />
                 </a>
               </td>
-              <td style={{ verticalAlign: 'top', width: 248, display: 'block' }}>
+              <td style={{ verticalAlign: 'top', width: 136, display: 'block' }}>
                 <p
                   style={{
                     fontSize: 12,
-                    lineHeight: 1.4,
+                    lineHeight: '1.4em',
                     textAlign: 'left',
                     margin: 0,
                     padding: '2px 6px',
@@ -62,7 +62,7 @@ export default function AdBanner({ type = 'default' }) {
                   }}
                 >
                   <a
-                    href="https://hb.afl.rakuten.co.jp/ichiba/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16497722%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIzMDB4MzAwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
+                    href="https://hb.afl.rakuten.co.jp/ichiba/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16497722%2F&link_type=picttext&ut=..."
                     target="_blank"
                     rel="nofollow sponsored noopener"
                     style={{ wordWrap: 'break-word' }}
@@ -70,49 +70,9 @@ export default function AdBanner({ type = 'default' }) {
                     はやぶさ2　最強ミッションの真実 （NHK出版新書　639　639） [ 津田 雄一 ]
                   </a>
                   <br />
-                  <span>価格：990円（税込、送料無料) </span>{' '}
+                  <span>価格：990円（税込、送料無料)</span>{' '}
                   <span style={{ color: '#BBB' }}>(2025/7/3時点)</span>
                 </p>
-                <div style={{ margin: 10 }}>
-                  <a
-                    href="https://hb.afl.rakuten.co.jp/ichiba/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16497722%2F&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIzMDB4MzAwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ%3D%3D"
-                    target="_blank"
-                    rel="nofollow sponsored noopener"
-                    style={{ wordWrap: 'break-word' }}
-                  >
-                    <img
-                      src="https://static.affiliate.rakuten.co.jp/makelink/rl.svg"
-                      alt="楽天"
-                      style={{ float: 'left', maxHeight: 27, width: 'auto', marginTop: 0 }}
-                    />
-                  </a>
-                  <a
-                    href="https://hb.afl.rakuten.co.jp/ichiba/49ec4b74.53ebf16c.49ec4b75.c45c8b88/?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fbook%2F16497722%2F%3Fscid%3Daf_pc_bbtn&link_type=picttext&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0dGV4dCIsInNpemUiOiIzMDB4MzAwIiwibmFtIjoxLCJuYW1wIjoicmlnaHQiLCJjb20iOjEsImNvbXAiOiJkb3duIiwicHJpY2UiOjEsImJvciI6MSwiY29sIjoxLCJiYnRuIjoxLCJwcm9kIjowLCJhbXAiOmZhbHNlfQ=="
-                    target="_blank"
-                    rel="nofollow sponsored noopener"
-                    style={{ wordWrap: 'break-word' }}
-                  >
-                    <div
-                      style={{
-                        float: 'right',
-                        width: '41%',
-                        height: 27,
-                        backgroundColor: '#bf0000',
-                        color: '#fff',
-                        fontSize: 12,
-                        fontWeight: 500,
-                        lineHeight: '27px',
-                        marginLeft: 1,
-                        padding: '0 12px',
-                        borderRadius: 16,
-                        cursor: 'pointer',
-                        textAlign: 'center',
-                      }}
-                    >
-                      楽天で購入
-                    </div>
-                  </a>
-                </div>
               </td>
             </tr>
           </tbody>
