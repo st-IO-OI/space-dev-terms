4e9f95f508ce8ba1573f8c806f5723ca79faa9f9
st-IO-OI <59336563+st-IO-OI@users.noreply.github.com>
2025-07-03 22:05:24 +0900

    07032130

--- src/theme/DocItem/Layout/index.js
+++ src/theme/DocItem/Layout/index.js
@@ -5,9 +5,11 @@ import TOC from '@theme/TOC';
 import DocItemContent from '@theme/DocItem/Content'; // 追加
 import AdBanner from '@site/src/components/AdsenseBanner';
 import useIsBrowser from '@docusaurus/useIsBrowser';
+import { useDoc } from '@docusaurus/theme-common/internal';
 
 export default function DocItemLayout({ children, frontMatter = {}, toc }) {
   const isBrowser = useIsBrowser();
+  const { frontMatter } = useDoc();
   const adType = frontMatter.ads ?? 'default';
 
   return (
